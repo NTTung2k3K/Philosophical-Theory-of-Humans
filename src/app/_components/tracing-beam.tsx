404c29318344d52d5fb1b4f6f27fcb6bc32e8974
Thang Lam <thanglam271003@gmail.com>
2025-01-15 14:50:34 +0700

    check ui phattrien

--- src/app/_components/tracing-beam.tsx
+++ src/app/_components/tracing-beam.tsx
@@ -10,11 +10,11 @@ export function TracingBeamDemo() {
       <div className="max-w-2xl mx-auto antialiased pt-4 relative">
         {dummyContent.map((item, index) => (
           <div key={`content-${index}`} className="mb-10">
-            <h2 className="bg-black text-white rounded-full text-md  w-fit px-4 py-1 mb-4">
+            <h2 className="bg-black text-white rounded-full text-lg  w-fit px-4 py-1 mb-4">
               {item.badge}
             </h2>
 
-            <p className={twMerge("text-3xl mb-4")}>{item.title}</p>
+            <p className={twMerge("text-4xl font-bold mb-4")}>{item.title}</p>
 
             <div className="text-lg prose prose-sm dark:prose-invert">
               {item?.image && (
