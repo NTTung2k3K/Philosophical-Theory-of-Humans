8f60202fc46d1796d20e998eb70d4a5ac314e6a3
Nguyễn Thanh Tùng <nttung2k3k@gmail.com>
2025-01-15 22:20:10 +0700

    Add preloader

--- src/app/_components/lazy-video.tsx
+++ src/app/_components/lazy-video.tsx
@@ -55,7 +55,7 @@ export default function LazyVideo({
       className={className}
       preload="none"
     >
-      <source src={src} type="video/mp4" />
+      <source src={src} type="video/webm" />
       Your browser does not support the video tag.
     </video>
   );
